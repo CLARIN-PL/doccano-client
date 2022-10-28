51f12febefb0ea5f8b042a42e997d6945a794bab
haanh764 <ngohaanh764@gmail.com>
2022-10-28 19:32:40 +0200

    add: demo

---
+++ demo/project_creator.py
@@ -0,0 +1,69 @@
+from doccano_client import DoccanoClient
+import os
+import jsonlines
+
+
+DATA_DIR = './datasets'
+LABEL_DIR = './labels_config'
+DATA_FORMAT = 'JSON'
+COLUMN_DATA ='text'
+COLUMN_LABEL ='label'
+
+USER_DATA = 'users.jsonl'
+USER_ROLE = 'annotator'
+PROJECT_TYPE = 'AffectiveAnnotation'
+
+ADMIN_NAME = 'admin'
+ADMIN_PASSWORD = 'password'
+BASE_URL = 'http://localhost:8000'
+ANNOTATION_MODES = ['OthersAnnotation', 'HumorAnnotation', 'OffensiveAnnotation', 'EmotionsAnnotation', 'SummaryAnnotation']
+
+LIST_DATA_FILES = os.listdir(DATA_DIR)
+LIST_LABEL_FILES = os.listdir(LABEL_DIR)
+LABELS_CONFIG_MAPPING = dict(zip(ANNOTATION_MODES[:-1], LIST_LABEL_FILES))
+
+client = DoccanoClient(base_url=BASE_URL)
+client.login(username=ADMIN_NAME, password=ADMIN_PASSWORD)
+
+
+def create_users():
+    list_users = []
+    with open(USER_DATA, 'r') as f:
+        for item in jsonlines.Reader(f):
+            list_users.append(item['username'])
+            try:
+                client.create_user(username=item['username'], password=item['password'])
+            except:
+                pass
+    print("Users created: {}".format(list_users))
+    return list_users
+
+
+def processing(idx, annotation_mode, usernames):
+    if annotation_mode=='SummaryAnnotation':
+        client.create_project(name="{}_{}".format(LIST_DATA_FILES[idx].split('.')[0], annotation_mode), project_type=PROJECT_TYPE, description="Affective Annotation Summary mode", is_summary_mode=True, is_single_ann_view=True)
+    elif annotation_mode=='EmotionsAnnotation':
+        client.create_project(name="{}_{}".format(LIST_DATA_FILES[idx].split('.')[0], annotation_mode), project_type=PROJECT_TYPE, description="Affective Annotation Emotions mode", is_emotions_mode=True, is_single_ann_view=True)
+    elif annotation_mode=='OffensiveAnnotation':
+        client.create_project(name="{}_{}".format(LIST_DATA_FILES[idx].split('.')[0], annotation_mode), project_type=PROJECT_TYPE, description="Affective Annotation Offensive mode", is_offensive_mode=True, is_single_ann_view=True)
+    elif annotation_mode=='HumorAnnotation':
+        client.create_project(name="{}_{}".format(LIST_DATA_FILES[idx].split('.')[0], annotation_mode), project_type=PROJECT_TYPE, description="Affective Annotation Humor mode", is_humor_mode=True, is_single_ann_view=True)
+    elif annotation_mode=='OthersAnnotation':
+        client.create_project(name="{}_{}".format(LIST_DATA_FILES[idx].split('.')[0], annotation_mode), project_type=PROJECT_TYPE, description="Affective Annotation Others mode", is_others_mode=True, is_single_ann_view=True)
+
+    current_project_id = list(client.list_projects())[-1].id
+    client.upload(project_id=current_project_id, file_paths=[os.path.join(DATA_DIR, LIST_DATA_FILES[idx])], task=PROJECT_TYPE, format=DATA_FORMAT, column_data=COLUMN_DATA, column_label=COLUMN_LABEL)
+    if annotation_mode!='SummaryAnnotation':
+        client.upload_label_type(project_id=current_project_id, file_path=os.path.join(LABEL_DIR, LABELS_CONFIG_MAPPING[annotation_mode]), type='scale')
+    for user in usernames:
+        client.add_member(project_id=current_project_id, username=user, role_name=USER_ROLE)
+
+def main():
+    usernames = create_users()
+    for idx in range(len(LIST_DATA_FILES)):
+        for annotation_mode in ANNOTATION_MODES:
+            processing(idx, annotation_mode, usernames)
+    print("Finished creating projects, users and uploading data, labels. Assigning users to projects...")
+
+if __name__ == '__main__':
+    main()
